eb96344a332f2c8d4e9be6820fc4e83a62a0f025
RoySamson-stack <rsam9009@gmail.com>
2022-02-11 11:08:10 +0300

    making the header component

---
+++ src/main/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <p>Keeper</p>
+    </div>
+  );
+}
+export default Header
