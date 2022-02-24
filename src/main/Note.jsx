1f08ac7085c57f772ac1ccaac7433195473e65f6
RoySamson-stack <rsam9009@gmail.com>
2022-02-24 14:17:06 +0300

    adding the delete functionality to the cards

--- src/main/Note.jsx
+++ src/main/Note.jsx
@@ -7,6 +7,7 @@ function Notes(props){
       <div className="note">
         <h1 className="note_title">{props.title}</h1>
         <p className="note_content">{props.content}</p>
+        <button>DELETE</button>
       </div>
     )
   }
